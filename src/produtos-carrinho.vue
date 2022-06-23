91a98eefdc38405fad41d9d63b7715b484b0dadc
Rafael-Corona <61752350+Rafael-Corona@users.noreply.github.com>
2022-06-23 20:22:44 -0300

    Create produtos-carrinho.vue

---
+++ src/produtos-carrinho.vue
@@ -0,0 +1,96 @@
+<template>
+
+<div class="container" id>
+  <div class="row align-items-center" v-for="(aula, index) in aulas" :key="aula.nome">
+    
+    <div class="col">
+      <img class="img-fluid" v-bind:src="aulas[index].image" :alt="aulas.nome" />
+    </div>
+
+    <div class="col">
+      {{aula.nome}}
+    </div>
+
+    <div class="col">
+      {{aula.price}}
+    </div>
+
+    <div class="col">
+      <div class="row">
+        <div class="col offset-3">
+          <button id="removeItem" type="button" class="btn btn-light">Remover</button> <br>
+                    <img class="img-fluid" id="trash-icon" :src="trashIcon"/>
+
+        </div>
+      </div>
+        
+
+        
+    </div>
+  </div>
+</div>
+
+
+</template>
+
+<script> 
+
+
+export default{
+    name: 'resumo_carrinho',
+    
+
+  data: ()=>{
+    return {
+      aulas: [
+        {
+          nome:'Álgebra Linear ',
+          price: 'R$ 120.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvidaconmascotas.com%2Fwp-content%2Fuploads%2F2021%2F01%2F1610477250_El-gato-habla-mucho-%25C2%25BFque-nos-quiere-decir-850x500.jpg&f=1&nofb=1',
+        },
+        {
+          nome:'Geometria Analítica ',
+          price: 'R$ 20.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.grouphealth.ca%2Fwp-content%2Fuploads%2F2018%2F05%2Fplaceholder-image-300x225.png&f=1&nofb=1',
+        },
+        {
+          nome:'Estatística Básica ',
+          price: 'R$ 220.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.grouphealth.ca%2Fwp-content%2Fuploads%2F2018%2F05%2Fplaceholder-image-300x225.png&f=1&nofb=1',
+        },
+        {
+          nome:'Cálculo 4 ',
+          price: 'R$ 120.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.grouphealth.ca%2Fwp-content%2Fuploads%2F2018%2F05%2Fplaceholder-image-300x225.png&f=1&nofb=1',
+        },
+        {
+          nome:'Cálculo 4 ',
+          price: 'R$ 120.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.grouphealth.ca%2Fwp-content%2Fuploads%2F2018%2F05%2Fplaceholder-image-300x225.png&f=1&nofb=1',
+        },
+        {
+          nome:'Cálculo 4 ',
+          price: 'R$ 120.00',
+          image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.grouphealth.ca%2Fwp-content%2Fuploads%2F2018%2F05%2Fplaceholder-image-300x225.png&f=1&nofb=1',
+        }
+      ],
+      
+      trashIcon: require('@/assets/trash-icon.png'),
+
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+.list-group{
+    width: 50rem;
+    height: 15rem;
+}
+#trash-icon{
+  width: 40%;
+}
+
+</style>
